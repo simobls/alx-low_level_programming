050405f3716625189cf7c5d6e9cf05e8522c0777
simobls <belassalsimo78@gmail.com>
2023-08-15 12:26:44 +0100

    add

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char text[9] = "_putchar";
+int i = 0;
+
+for (i = 0; i < 8; i++)
+{
+putchar(text[i]);
+}
+putchar('\n');
+return (0);
+}
