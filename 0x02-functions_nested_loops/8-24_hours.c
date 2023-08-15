1230c7d129e74b41ecab75478ef3b817ad8c9bba
simobls <belassalsimo78@gmail.com>
2023-08-15 20:43:27 +0100

    add

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,10 +3,11 @@
 void jack_bauer(void)
 {
     int htens, hones, mtens, mones, hmax;
+    htens = 0, hones = 0, mtens = 0, mones = 0;
     while (htens < 3)
     {
         hmax = 9;
-        if (htens = 2)
+        if (htens == 2)
         {
             hmax = 3;
         }
