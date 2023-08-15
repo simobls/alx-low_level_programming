1bfedb001a093797b663eee5d267c74ac6c63f24
simobls <belassalsimo78@gmail.com>
2023-08-15 14:01:19 +0100

    add

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,11 @@
 
 int _islower(int c)
 {
-if ( ( c >= 'a' && c <= 'z' ) || ( c >= 'A' && c <= 'Z' ) )
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
