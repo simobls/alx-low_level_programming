f35edd7b5984283b62bdd0a95200964c41312608
simobls <belassalsimo78@gmail.com>
2023-08-15 13:57:52 +0100

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for alphabet
+ * @c: c is an ascii character
+ *
+ * Return: 1 if alphabetic
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
