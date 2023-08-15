387d34bb884fe6da75299edecacacc31a7914aaa
simobls <belassalsimo78@gmail.com>
2023-08-15 21:09:18 +0100

    add

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,16 +10,28 @@
 
 void times_table(void)
 {
-int i = 0, j, n;
+int i = 0, j, n, tens, ones;
 while (i < 10)
 {
 j = 0;
 while (j < 10)
 {
 n = i * j;
+tens = n / 10;
+ones = n % 10;
+if (n < 10)
+{
 _putchar(n + '0');
 _putchar(',');
 _putchar(' ');
+}
+else if (n > 10)
+{
+_putchar(tens + '0');
+_putchar(ones + '0');
+_putchar(',');
+_putchar(' ');
+}
 j++;
 }
 _putchar('\n');
