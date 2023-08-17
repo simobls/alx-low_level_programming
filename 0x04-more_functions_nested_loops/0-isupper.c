511dbe8138bb341a796a046ad13b52e4cbc0e941
simobls <belassalsimo78@gmail.com>
2023-08-17 19:33:42 +0100

    add

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,16 +5,19 @@
  * @c: An input character
  * Return: 1 if c is uppercase or 0 otherwise
  */
-
 int _isupper(int c)
 {
-int i;
-for (i = 'A'; i < 'Z'; i++)
-{
-if (c == i)
-{
-return (1);
-}
-}
-return (0);
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
 }
