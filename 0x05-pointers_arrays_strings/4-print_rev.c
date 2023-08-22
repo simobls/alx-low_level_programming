9a6035716f4244dd8f6b5ee23651f1809dc35772
simobls <belassalsimo78@gmail.com>
2023-08-22 20:44:49 +0100

    add

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,11 +12,11 @@ void print_rev(char *s)
 	int i, j = 0, len;
 	char *rev;
 
-	len = _strlen(*s);
+	len = _strlen(s);
 	for (i = len - 1; i >= 0; i--)
 	{
 		rev[j] = s[i];
 		j++;
 	}
-	_puts(*rev);
+	_puts(rev);
 }
