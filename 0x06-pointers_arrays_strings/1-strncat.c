87db9ec2a39d4b5b351829a8435b4e4dbf0d8c74
simobls <belassalsimo78@gmail.com>
2023-08-24 15:38:26 +0100

    add

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,6 +17,10 @@ char *_strncat(char *dest, char *src, int n)
 	while (count2 <= n)
 	{
 		*(dest + count1) = *(src + count2);
+		if (*(src + count2) == '\0')
+		{
+			break;
+		}
 		count1++;
 		count2++;
 	}
