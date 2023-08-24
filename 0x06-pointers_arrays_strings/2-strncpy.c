031460f166da7c889a4737d44b46564c0b3a08cb
simobls <belassalsimo78@gmail.com>
2023-08-24 21:04:06 +0100

    add

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strncpy - copies a string
  * @dest: destination.
@@ -10,12 +9,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int count1 = 0;
+	int i;
 
-	while (count1 < n && src[count1] != '\0')
-	{
-		dest[count1] = src[count1];
-		count1++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
