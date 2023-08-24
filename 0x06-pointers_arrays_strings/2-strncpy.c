7dfefa1b216a12c3d3de27ab9f271661ecbfb64d
simobls <belassalsimo78@gmail.com>
2023-08-24 21:02:58 +0100

    add

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,13 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int count1 = 0;
 
-	while (count1 < n)
+	while (count1 < n && src[count1] != '\0')
 	{
-		*(dest + count1) = *(src + count1);
-		if (*(src + count1) == '\0')
-		{
-			break;
-		}
+		dest[count1] = src[count1];
 		count1++;
 	}
 	return (dest);
