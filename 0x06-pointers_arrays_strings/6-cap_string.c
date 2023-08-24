e2a7692b5b2d958f8d8baf44c8adbb2c7dcfde7a
simobls <belassalsimo78@gmail.com>
2023-08-25 00:45:00 +0100

    add

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words of a string
- * @a: input string.
+ * @s: input string.
  * Return: the pointer to dest.
  */
 
-char *cap_string(char *a)
+char *cap_string(char *s)
 {
-	int count = 0, count2;
-	int sep[] = {32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125, 9, 10};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (*(a + count) != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		for (count2 = 0; count2 < 13; count2++)
+		for (i = 0; i < 13; i++)
 		{
-			if (*(a + count) == sep[count2])
+			if (*(s + count) == sep_words[i])
 			{
-				if ((*(a + (count + 1)) >= 97) && (*(a + (count + 1)) <= 122))
-					*(a + count + 1) = *(a + count + 1) - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
 		}
 		count++;
 	}
-	return (a);
+	return (s);
 }
