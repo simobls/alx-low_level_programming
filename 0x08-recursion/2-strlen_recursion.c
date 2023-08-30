d3a47b68ac1da1f680561a92f17c830ccb202c0a
simobls <belassalsimo78@gmail.com>
2023-08-30 14:27:55 +0100

    add

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
+
 /**
  * _strlen_recursion - size
  * @s: pointer to string params
  * Return: recursion
  */
-
 int _strlen_recursion(char *s)
 {
 	int len = 0;
 
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
 		len++;
+		_strlen_recursion(s + len);
 	}
 	return (len);
 }
