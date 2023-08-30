e8efd01560df6429960a6c33c93244b70a5ef342
simobls <belassalsimo78@gmail.com>
2023-08-30 13:54:47 +0100

    add

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse
+ * @s: pointer to string params
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
