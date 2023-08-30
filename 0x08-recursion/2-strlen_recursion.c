f735af4c1b939d357a12f495ee6499f7a758ab08
simobls <belassalsimo78@gmail.com>
2023-08-30 14:26:15 +0100

    add

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	int *len = 0;
+	int len = 0;
 
 	if (*s != '\0')
 	{
-		*len++;
-		_strlen_recursion(++s);
+		_strlen_recursion(s + 1);
+		len++;
 	}
-	return (*len);
+	return (len);
 }
