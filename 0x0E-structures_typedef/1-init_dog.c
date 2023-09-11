7a44edef02762e7f6c68c7492271c6115c3831b8
simobls <belassalsimo78@gmail.com>
2023-09-11 14:54:50 +0100

    add

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -16,7 +16,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	{
 		return;
 	}
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
-}
+	else
+	{
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
+	}
+	}
