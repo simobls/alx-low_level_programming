1cfa2f61b8b8036d1133954332da2bb1416246b6
simobls <belassalsimo78@gmail.com>
2023-09-21 15:34:50 +0100

    add

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
