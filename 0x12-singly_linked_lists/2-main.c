981a8fc8ad3815c5be74d160ad1dae95477cf241
simobls <belassalsimo78@gmail.com>
2023-09-21 16:33:36 +0100

    add

---
+++ 0x12-singly_linked_lists/2-main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    list_t *head;
+
+    head = NULL;
+    add_node(&head, "Alexandro");
+    add_node(&head, "Asaia");
+    add_node(&head, "Augustin");
+    add_node(&head, "Bennett");
+    print_list(head);
+    return (0);
+}
