5f806df6f151b0afd937e585af472c161425d482
simobls <belassalsimo78@gmail.com>
2023-09-26 16:06:30 +0100

    add

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,12 +9,11 @@
  * Return: address of the new element. NUll if it failed.
  */
 
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
