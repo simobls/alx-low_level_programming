11d6f3fca67bb665e5d038d60885f92a4a3b3634
simobls <belassalsimo78@gmail.com>
2023-09-26 12:03:02 +0100

    add

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,15 +7,15 @@
  * Return: numbers of nodes.
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
 		count++;
+		*h = h->next;
+		
 	}
 	return (count);
 }
