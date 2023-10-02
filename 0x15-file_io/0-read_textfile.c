97603ad6495e1201acedfe61a18aba5831734af9
simobls <belassalsimo78@gmail.com>
2023-10-02 20:30:22 +0100

    add

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	size_t nread, nwrite;
+	char* ptr;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	ptr = malloc(letters);
+
+	nread = read(fd, ptr, letters);
+	nwrite = write(STDOUT_FILENO, ptr, nread);
+
+	close(fd);
+
+	free(ptr);
+	return (nwrite);
+}
