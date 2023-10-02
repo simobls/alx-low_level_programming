ec45f5f56e7717aae9dae46ad64f436ba8dd47f5
simobls <belassalsimo78@gmail.com>
2023-10-02 20:33:24 +0100

    add

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -10,26 +7,30 @@
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	ssize_t nread, nwrite;
-	char *ptr;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	ptr = malloc(letters);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	nread = read(fd, ptr, letters);
-	nwrite = write(STDOUT_FILENO, ptr, nread);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
-	free(ptr);
-	return (nwrite);
+	free(buf);
+
+	return (nwr);
 }
